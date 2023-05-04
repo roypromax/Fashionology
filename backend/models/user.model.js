6aa6e3dbdd8a5b4aead73f6653b6710e25ee17b3
roypromax <roy19rishabh@gmail.com>
2023-05-04 15:53:03 +0530

    user register and login routes

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -4,7 +4,7 @@ const userSchema = mongoose.Schema({
     name:{ type: String, required: true },
     email:{ type: String, required: true, unique: true },
     password:{ type: String, required: true },
-    role:{ type: String, enum: ["user", "admin"], default: "user" },
+    role:{ type: String, enum: ["user","admin"], default: "user" },
     address:{ type: String, required:true },
     phoneNumber:{ type: Number, required:true }
 },
@@ -12,6 +12,6 @@ const userSchema = mongoose.Schema({
     versionKey:false
 });
 
-const UserModel = mongoose.model("user", userSchema);
+const UserModel = mongoose.model("user",userSchema);
 
 module.exports = { UserModel };
---
+++ backend/routes/user.route.js
@@ -0,0 +1,51 @@
+const express = require("express");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
+const UserModel = require("../models/user.model");
+
+const userRouter = express.Router();
+
+
+userRouter.post("/register",(req,res) => {
+    const {password} = req.body;
+    
+    bcrypt.hash(password, 5, async(err, hash)=>{
+        try {
+            const newUser = new UserModel({...req.body,password:hash});
+            await newUser.save();
+            res.status(200).json({msg:"New user has been registered"});
+        } catch (error) {
+            res.status(400).json({err:error});
+        }
+    });   
+});
+
+
+userRouter.post("/login",async(req,res)=>{
+
+    const {email,password} = req.body;
+
+  try {
+    
+    const user = await UserModel.findOne({email:email});
+
+    if (!user) {
+        res.status(400).json({msg:"Invalid email or password"});
+    }else{
+        bcrypt.compare(password, user.password, (err,result)=>{
+            if(err){
+                res.status(400).json({msg:"Invalid email or password"});
+            }else{
+                const token = jwt.sign({userId:user._id,userName:user.name},process.env.jwtSecret);
+                res.status(200).json({msg:"Login Successful",token:token});
+            }
+        });
+    }
+    } catch (err) {
+        console.error(err);
+        res.status(400).json({msg:"Error logging in"});
+  }
+});
+
+module.exports = {userRouter};
