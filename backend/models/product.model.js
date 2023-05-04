9c43150302cdbb2844d33bd5abc1e6e249bd57aa
roypromax <roy19rishabh@gmail.com>
2023-05-04 14:00:32 +0530

    product model

---
+++ backend/models/product.model.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+
+const productSchema = mongoose.Schema({
+    name:{type:String,required:true},
+    rating:{type:Number,required:true},
+    price:{type:Number,required:true},
+    sizes:{type:Array,required:true},
+    quantity:{type:Number,required:true},
+    color:{type:String,required:true},
+    mainCategory:{type:String,required:true},
+    subCategory:{type:String,required:true},
+    images:{type:Array,required:true},
+    brand:{type:String,required:true},
+    description:{type:String,required:true},
+},{
+    versionKey:false
+})
+
+const ProductModel = mongoose.model("product",productSchema);
+
+module.exports = {ProductModel};
